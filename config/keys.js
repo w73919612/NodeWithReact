3077a61625471e6050437f433ccc93d7f7ae04df
w73919612 <kofecwi@gmail.com>
2018-12-02 13:55:56 -0500

    26. Access and Refresh Tokens finished

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '963331489181-8f00dm06i1nsps0koraimt1icjil1an9.apps.googleusercontent.com',
+  googleClientSecret: 'wVeYHNF67u_1GrZithp4wTeL'
+}
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy'}); 
-});
+// app.get('/', (req, res) => {
+//   res.send({ bye: 'buddy'});
+// });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+          console.log('profile:', profile);
+    }
+  )
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));  //GoogleStrategy has this thing where its looking for keyword 'google'
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
