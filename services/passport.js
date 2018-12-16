ea2f795885a31b4ca51586efc74aed5293a2722c
w73919612 <kofecwi@gmail.com>
2018-12-15 19:46:23 -0500

    Beginning of React and Redux code. Hi There prints out in browser.

--- services/passport.js
+++ services/passport.js
@@ -44,18 +44,31 @@ passport.use(
       callbackURL: keys.googleRedirectURI + '/auth/google/callback'
       //proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id})
-        .then((existingUser) => {
-          if (existingUser) {
-            // user already exists
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        });
+    // (accessToken, refreshToken, profile, done) => {
+    //   User.findOne({ googleId: profile.id})
+    //     .then((existingUser) => {
+    //       if (existingUser) {
+    //         // user already exists
+    //         done(null, existingUser);
+    //       } else {
+    //         new User({ googleId: profile.id })
+    //           .save()
+    //           .then(user => done(null, user));
+    //       }
+    //     });
+
+
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id});
+      if (existingUser) {
+        // user already exists
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+      }
+
+
       //console.log('access token', accessToken);
       //console.log('refresh token', refreshToken);
       console.log('profile:', profile);
