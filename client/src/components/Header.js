a2a3cb18d5caeafddd15fcd490700f9fe0a33a54
w73919612 <kofecwi@gmail.com>
2018-12-17 19:15:31 -0500

    End of Lesson 87 - Which concludes Section 7 of Course. However, the Error from lesson 84 is still not solved: Error-the surveys gets appended to port5000 not 3000.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from  'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -10,17 +11,19 @@ renderContent() {
     case false:
       return <li><a href="/auth/google">Login with Google</a></li>;
     default:
-      return <li><a href="/auth">Logout</a></li>;
+      return <li><a href="/api/logout">Logout</a></li>;
   }
 }
   render() {
-    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
