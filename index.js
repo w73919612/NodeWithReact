fd34e69207d41dc5fe5055d9b39ec30be8e533be
w73919612 <kofecwi@gmail.com>
2018-12-21 18:51:31 -0500

    End of Lecture 103. Working.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/users')
 require('./services/passport.js');
 
@@ -11,6 +12,9 @@ const app = express();
 // app.get('/', (req, res) => {
 //   res.send({ bye: 'buddy'});
 // });
+
+app.use(bodyParser.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -30,6 +34,7 @@ require('./routes/authRoutes')(app); /* the require statement essentially
                                        returns the function from authRoutes.js,
                                        and then the (app) gets provided as the
                                        argument for that function. */
+require('./routes/billingRoutes')(app);
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
